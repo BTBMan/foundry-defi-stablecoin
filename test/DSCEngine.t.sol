fdbe5de2e4e2f9c0db32c5db28fa94f3be05f301
BTBMan <1498118157@qq.com>
2024-10-06 17:51:57 +0800

    test: constructor
    Signed-off-by: BTBMan <1498118157@qq.com>

--- test/DSCEngine.t.sol
+++ test/DSCEngine.t.sol
@@ -9,20 +9,36 @@ import {DSCEngineScript} from "../script/DSCEngine.s.sol";
 import {HelperConfig} from "../script/HelperConfig.s.sol";
 
 contract DSCEngineTest is Test {
+    HelperConfig.NetworkConfig public activeNetworkConfig;
+
     uint256 public constant AMOUNT_COLLATERAL = 10 ether;
     uint256 public constant STARTING_ERC20_BALANCE = 10 ether;
+
+    address[] public tokenAddresses;
+    address[] public priceFeedAddresses;
+
     DSCEngine public dscEngine;
     DecentralizedStablecoin public decentralizedStablecoin;
-    HelperConfig.NetworkConfig public activeNetworkConfig;
+    DSCEngineScript public dscEngineScript;
 
     address public user = makeAddr("user");
 
     function setUp() public {
-        (decentralizedStablecoin, dscEngine, activeNetworkConfig) = new DSCEngineScript().run();
+        dscEngineScript = new DSCEngineScript();
+        (decentralizedStablecoin, dscEngine, activeNetworkConfig) = dscEngineScript.run();
         ERC20Mock(activeNetworkConfig.weth).mint(user, STARTING_ERC20_BALANCE);
     }
 
-    function testGetUsdValue() public view {
+    function testRevertsIfTokenLengthDoesntMatchPriceFeed() public {
+        tokenAddresses.push(activeNetworkConfig.weth);
+        priceFeedAddresses.push(activeNetworkConfig.wethUSDPriceFeed);
+        priceFeedAddresses.push(activeNetworkConfig.wbtcUSDPriceFeed);
+
+        vm.expectRevert(DSCEngine.DSCEngine__TokenAddressesAndPriceFeedAddressesMustBeSameLength.selector);
+        new DSCEngine(tokenAddresses, priceFeedAddresses, address(decentralizedStablecoin));
+    }
+
+    function testGetUSDValue() public view {
         uint256 ethAmount = 15e18;
         uint256 expectedUsd = 30000e18;
         uint256 actualUsd = dscEngine.getUSDValue(activeNetworkConfig.weth, ethAmount);
