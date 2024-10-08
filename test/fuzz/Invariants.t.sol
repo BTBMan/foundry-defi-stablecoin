801b775be40512c69484f83b40fe1c04358c63e1
BTBMan <1498118157@qq.com>
2024-10-08 12:51:33 +0800

    test: InvariantTest
    Signed-off-by: BTBMan <1498118157@qq.com>

--- test/fuzz/Invariants.t.sol
+++ test/fuzz/Invariants.t.sol
@@ -1,8 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.27;
 
-import {Test} from "forge-std/Test.sol";
+import {Test, console} from "forge-std/Test.sol";
 import {StdInvariant} from "forge-std/StdInvariant.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {DSCEngine} from "../../src/DSCEngine.sol";
 import {DecentralizedStablecoin} from "../../src/DecentralizedStablecoin.sol";
 import {DSCEngineScript} from "../../script/DSCEngine.s.sol";
@@ -13,18 +14,28 @@ import {HelperConfig, IHelperConfig} from "../../script/HelperConfig.s.sol";
 // 1. The total supply of DSC should be always less than the total value of collateral.
 // 2. Getter view functions should never revert.
 
-contract InvariantsTest is StdInvariant, Test {
+contract InvariantTest is StdInvariant, Test, IHelperConfig {
+    NetworkConfig public activeNetworkConfig;
+
     DSCEngine public dscEngine;
     DecentralizedStablecoin public decentralizedStablecoin;
     HelperConfig public helperConfig;
 
     function setUp() public {
         (decentralizedStablecoin, dscEngine, helperConfig) = new DSCEngineScript().run();
+        activeNetworkConfig = helperConfig.getActiveNetworkConfig();
 
         targetContract(address(dscEngine));
     }
 
-    function invariant_DSCMustHaveMoreValueThanTotalSupply() public pure {
-        assertEq(true, true);
+    function invariant_DSCMustHaveMoreValueThanTotalSupply() public view {
+        uint256 totalSupply = decentralizedStablecoin.totalSupply();
+        uint256 totalWETHDeposited = IERC20(activeNetworkConfig.weth).balanceOf(address(dscEngine));
+        uint256 totalWBTCDeposited = IERC20(activeNetworkConfig.wbtc).balanceOf(address(dscEngine));
+
+        uint256 wethValue = dscEngine.getUSDValue(activeNetworkConfig.weth, totalWETHDeposited);
+        uint256 wbtcValue = dscEngine.getUSDValue(activeNetworkConfig.wbtc, totalWBTCDeposited);
+
+        assertLe(totalSupply, wethValue + wbtcValue);
     }
 }
